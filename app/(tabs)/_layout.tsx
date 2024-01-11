ef397352a4d88092288a9e5117d7ad9ded0a0167
rammah <ramahnore@gmail.com>
2024-01-11 16:51:37 +0400

    add navigation tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,56 @@
 import React from "react";
 import { Tabs } from "expo-router";
+import { Image, StyleSheet } from "react-native";
+import {
+	coin2,
+	dashboard,
+	home,
+	setting,
+} from "../../assets/icons";
 
 const RootLayout = () => {
 	return (
 		<Tabs>
-			<Tabs.Screen name="index" options={{ title: "Home" }} />
-			<Tabs.Screen name="stats/index" options={{ title: "Stats" }} />
-			<Tabs.Screen name="budgets/index" options={{ title: "Budgets" }} />
-			<Tabs.Screen name="settings/index" options={{ title: "Settings" }} />
+			<Tabs.Screen
+				name="index"
+				options={{
+					title: "Home",
+					tabBarIcon(props) {
+						return <Image source={home} width={20} style={styles.icon} />;
+					},
+				}}
+			/>
+			<Tabs.Screen
+				name="stats/index"
+				options={{
+					title: "Stats",
+					tabBarIcon(props) {
+						return <Image source={dashboard} style={styles.icon} />;
+					},
+				}}
+			/>
+			<Tabs.Screen
+				name="budgets/index"
+				options={{
+					title: "Budgets",
+					tabBarIcon(props) {
+						return <Image source={coin2} style={styles.icon} />;
+					},
+				}}
+			/>
+			<Tabs.Screen
+				name="settings/index"
+				options={{
+					title: "Settings",
+					tabBarIcon(props) {
+						return <Image source={setting} style={styles.icon} />;
+					},
+				}}
+			/>
 		</Tabs>
 	);
 };
 
 export default RootLayout;
+
+const styles = StyleSheet.create({ icon: { width: 20, height: 20 } });
