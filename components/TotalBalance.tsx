c12b664614f7ef1244bf6c27b3bb98954451d6d5
rammah <ramahnore@gmail.com>
2024-01-17 17:19:32 +0400

    create total balance component

---
+++ components/TotalBalance.tsx
@@ -0,0 +1,46 @@
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Colors, Typography } from "../constants";
+import { dollar1 } from "../assets/icons";
+import { TextStroke } from "./ui/TextStroke";
+
+const TotalBalance = () => {
+	return (
+		<View style={styles.container}>
+			<TextStroke stroke={1} color="black">
+				<Text style={styles.text}>Total Balance</Text>
+			</TextStroke>
+			<ImageBackground source={dollar1} style={styles.dollar} />
+			<TextStroke stroke={1} color="black">
+				<Text style={styles.text}>$23,560</Text>
+			</TextStroke>
+		</View>
+	);
+};
+
+export default TotalBalance;
+
+const styles = StyleSheet.create({
+	container: {
+		borderTopWidth: 2,
+		borderLeftWidth: 2,
+		borderRightWidth: 2,
+		borderBottomWidth: 2,
+		borderColor: Colors.darkest,
+		backgroundColor: Colors.primary["500"],
+		borderRadius: 60,
+		height: 60,
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+		paddingRight: 10,
+	},
+	dollar: {
+		width: 40,
+		height: 40,
+	},
+	text: {
+		color: Colors.offwhite,
+		fontFamily: Typography.fonts.bold,
+	},
+});
